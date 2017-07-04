4140db9d3d47773b184999d1b12cdec94afd92f2
Apple <apple@AppledeiMac.local>
2017-07-04 17:08:01 +0800

    Initial Commit

---
+++ YY团购/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YY团购
+//
+//  Created by Apple on 2017/7/4.
+//  Copyright © 2017年 Young41. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YY团购/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YY团购
+//
+//  Created by Apple on 2017/7/4.
+//  Copyright © 2017年 Young41. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
